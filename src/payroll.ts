930255b5cd46c4a280064dadffa1b6b50b0d5b22
Danial <145651640+DanialNajafi@users.noreply.github.com>
2025-05-01 09:05:15 +0200

    Update payroll.ts

--- src/payroll.ts
+++ src/payroll.ts
@@ -22,13 +22,46 @@ export type Payslip = {
   net: number;
 };
 
+function getAHVStartDate(born: Date): Date{
+return new Date(born.getFullYear() + 18, 0, 1);
+}
+
 export function calculatePayslip(salary: Salary): Payslip {
   // TODO: implement
-  const result: Payslip = {
-    salary: salary,
-    deductions: new Map(),
-    totalDeductions: 0.0,
-    net: salary.gross,
-  };
-  return result;
+
+const yearly = salary.gross * 12;
+const over17 = salary.payday >= getAHVStartDate(salary.born);
+
+const deductions: Deductions = new Map();
+
+if (over17) {
+
+deductions.set("AHV", salary.gross * DEDUCTION_RATES.get("AHV")! / 100);
+deductions.set("IV", salary.gross * DEDUCTION_RATES.get("IV")! / 100);
+deductions.set("EO", salary.gross * DEDUCTION_RATES.get("EO")! / 100);
+}
+
+if (yearly >= 2500) {
+deductions.set("ALV", salary.gross * DEDUCTION_RATES.get("ALV")! / 100);
+deductions.set("NBU", salary.gross * DEDUCTION_RATES.get("NBU")! / 100);
+}
+
+if (yearly >= 22680) {
+deductions.set("PK", salary.gross * DEDUCTION_RATES.get("PK")! / 100);
+}
+
+let total = 0;
+
+for (const v of deductions.values()){
+
+total += v;
+
+}
+
+const net = salary.gross - total;
+
+return {
+salary, deductions, totalDeductions: total, net
+};
+
 }
